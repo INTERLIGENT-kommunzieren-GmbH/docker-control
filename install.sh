f85d664a28c1fbff85c5451cd38d44d53fba058a
Tito Duarte <duartito@gmail.com>
2025-08-12 14:12:23 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run --rm -v "$(pwd)":"$(pwd)" -v "$HOME/.docker/cli-plugins":"/cli-plugins" -it -u $(id -u):$(id -g) ik/docker-plugin install-plugin
+docker run --rm -v "$HOME/.docker/cli-plugins":"/cli-plugins" -it -u $(id -u):$(id -g) ik/docker-plugin install-plugin
