90b173a9c2cc4f81c742ce1547a99c7d8c4e7ff7
Tito Duarte <duartito@gmail.com>
2025-08-12 11:53:26 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run --rm -e HOME=/home/$USER -v "$HOME":/home/$USER -it -u $(id -u):$(id -g) ik/docker-plugin install-plugin
+docker run --rm -v "$(pwd)":"$(pwd)" -v "$HOME/.docker/cli-plugins":"/cli-plugins" -it -u $(id -u):$(id -g) ik/docker-plugin install-plugin
